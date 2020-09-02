d4e46ef132f12e9feca0526d7fea928d609d37f0
Olexander <ssolexa@gmail.com>
2020-09-02 22:04:02 +0300

    popup fix

--- js/popup.js
+++ js/popup.js
@@ -6,8 +6,8 @@ $('.js-btnCallback').on('click', function(event){
             <form class="popup__form" method="post" action="mail.php">\
                 <h3 class="popup__form-title">Заполните форму,</h3>\
                 <div class="form-text">и наш специалист свяжется с Вами в течении 15 минут</div>\
-                <input class="popup__form-input" type="text" placeholder="Ваше имя">\
-                <input class="popup__form-input" type="tel" placeholder="Ваш телефон">\
+                <input class="popup__form-input" name="name" type="text" placeholder="Ваше имя" required>\
+                <input class="popup__form-input" name="tel" type="tel" placeholder="Ваш телефон" required>\
                 <button class="js-callback btn form-btn" type="submit">Давайте мы перезвоним</button>\
             </form>\
             <div class="close-popup js-close-popup"></div>\
@@ -22,8 +22,8 @@ $('.js-btnDiagnostik').on('click', function(event){
             <form class="popup__form" method="post" action="mail.php">\
                 <h3 class="popup__form-title">Заполните форму,</h3>\
                 <div class="form-text">и наш специалист свяжется с Вами в течении 15 минут для уточнения всех деталей</div>\
-                <input class="popup__form-input" type="text" placeholder="Ваше имя">\
-                <input class="popup__form-input" type="tel" placeholder="Ваш телефон">\
+                <input class="popup__form-input" name="name" type="text" placeholder="Ваше имя" required>\
+                <input class="popup__form-input" name="tel" type="tel" placeholder="Ваш телефон" required>\
                 <button class="js-callback btn form-btn" type="submit">Записаться на диагностику зрения</button>\
             </form>\
             <div class="close-popup js-close-popup"></div>\
@@ -38,8 +38,8 @@ $('.js-btnGlasses').on('click', function(event){
             <form class="popup__form" method="post" action="mail.php">\
                 <h3 class="popup__form-title">Заполните форму,</h3>\
                 <div class="form-text">чтобы узнать стоимость, и наш менеджер свяжется с Вами в течении 15 минут</div>\
-                <input class="popup__form-input" type="text" placeholder="Ваше имя">\
-                <input class="popup__form-input" type="tel" placeholder="Ваш телефон">\
+                <input class="popup__form-input" name="name" type="text" placeholder="Ваше имя" required>\
+                <input class="popup__form-input" name="tel" type="tel" placeholder="Ваш телефон" required>\
                 <button class="js-callback btn form-btn" type="submit">Узнать стоимость</button>\
             </form>\
             <div class="close-popup js-close-popup"></div>\
@@ -54,8 +54,8 @@ $('.js-btnAxessories').on('click', function(event){
             <form class="popup__form" method="post" action="mail.php">\
                 <h3 class="popup__form-title">Заполните форму,</h3>\
                 <div class="form-text">чтобы подобрать аксессуары для очков и линз, и наш менеджер свяжется с Вами в течении 15 минут</div>\
-                <input class="popup__form-input" type="text" placeholder="Ваше имя">\
-                <input class="popup__form-input" type="tel" placeholder="Ваш телефон">\
+                <input class="popup__form-input" name="name" type="text" placeholder="Ваше имя" required>\
+                <input class="popup__form-input" name="tel" type="tel" placeholder="Ваш телефон" required>\
                 <button class="js-callback btn form-btn" type="submit">Узнать стоимость</button>\
             </form>\
             <div class="close-popup js-close-popup"></div>\
@@ -70,8 +70,8 @@ $('.js-btnSpectacles').on('click', function(event){
             <form class="popup__form" method="post" action="mail.php">\
                 <h3 class="popup__form-title">Заполните форму,</h3>\
                 <div class="form-text">чтобы подобрать себе очки, и наш менеджер свяжется с Вами в течении 15 минут</div>\
-                <input class="popup__form-input" type="text" placeholder="Ваше имя">\
-                <input class="popup__form-input" type="tel" placeholder="Ваш телефон">\
+                <input class="popup__form-input" name="name" type="text" placeholder="Ваше имя" required>\
+                <input class="popup__form-input" name="tel" type="tel" placeholder="Ваш телефон" required>\
                 <button class="js-callback btn form-btn" type="submit">Подобрать себе очки</button>\
             </form>\
             <div class="close-popup js-close-popup"></div>\
@@ -86,8 +86,8 @@ $('.js-btnTeam').on('click', function(event){
             <form class="popup__form" method="post" action="mail.php">\
                 <h3 class="popup__form-title">Заполните форму,</h3>\
                 <div class="form-text">чтобы записаться на прием к специалисту, и наш менеджер свяжется с Вами в течении 15 минут, чтобы уточнить все детали </div>\
-                <input class="popup__form-input" type="text" placeholder="Ваше имя">\
-                <input class="popup__form-input" type="tel" placeholder="Ваш телефон">\
+                <input class="popup__form-input" name="name" type="text" placeholder="Ваше имя" required>\
+                <input class="popup__form-input" name="tel" type="tel" placeholder="Ваш телефон" required>\
                 <button class="js-callback btn form-btn" type="submit">Записаться на прием</button>\
             </form>\
             <div class="close-popup js-close-popup"></div>\
@@ -102,8 +102,8 @@ $('.js-btnQuestion').on('click', function(event){
             <form class="popup__form" method="post" action="mail.php">\
                 <h3 class="popup__form-title">Заполните форму,</h3>\
                 <div class="form-text">и наш специалист ответит на все Ваши вопросы в течении 15 минут</div>\
-                <input class="popup__form-input" type="text" placeholder="Ваше имя">\
-                <input class="popup__form-input" type="tel" placeholder="Ваш телефон">\
+                <input class="popup__form-input" name="name" type="text" placeholder="Ваше имя" required>\
+                <input class="popup__form-input" name="tel" type="tel" placeholder="Ваш телефон" required>\
                 <button class="js-callback btn form-btn" type="submit">Задать вопрос</button>\
             </form>\
             <div class="close-popup js-close-popup"></div>\
